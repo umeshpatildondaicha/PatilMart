599cc22c9a1828591964a4a00579b5f446916d81
Umesh Patil <52061782+umeshpatildondaicha@users.noreply.github.com>
2024-02-27 22:40:53 +0530

    7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { lazy, Suspense } from "react";
-import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/Navbar/Navbar";
