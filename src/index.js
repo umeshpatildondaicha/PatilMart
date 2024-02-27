df54304b6f2699147a41f8e8fe447cd8d4b1806e
Umesh Patil <52061782+umeshpatildondaicha@users.noreply.github.com>
2024-02-27 22:06:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,13 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 reportWebVitals();
