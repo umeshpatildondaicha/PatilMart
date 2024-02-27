b23dbdcb163a942d9f8bd348170af910b8c66f38
Umesh Patil <52061782+umeshpatildondaicha@users.noreply.github.com>
2024-02-27 21:55:26 +0530

    5

--- src/index.js
+++ src/index.js
@@ -7,16 +7,14 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
 
 reportWebVitals();
